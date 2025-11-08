f29b87d1f9cd3d73ba5e352b289b2d7a5ceec84a
Hari-repo <hariharanuk810@gmail.com>
2025-11-08 22:40:32 +0530

    Student comp created and added

---
+++ src/comp/Student.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Student = (props) => {
+    return(
+        <>        
+        <h4>Name :{props.name}</h4>
+        <h4>Class :{props.class}</h4>
+        <h4>Gender :{props.age}</h4>
+        </>
+
+    )
+}
+
+export default Student
